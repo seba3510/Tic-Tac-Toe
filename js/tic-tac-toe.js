39869fd4cf1c434266f78f62b279b71d34b54163
seba3510 <seba3510@protonmail.com>
2025-01-19 12:48:13 -0400

    feat: add 'resetBoard' function

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,11 +1,12 @@
 function GameBoard() {
 	const board = [
 		["X", "O", "X"],
-		["X", "O", "X"],
+		["X", "X", "X"],
 		["X", "O", "X"],
 	];
 
 	const printBoard = function printBoard() {
+
 		const boardDisplay = `
     ${board[0][0]} | ${board[0][1]} | ${board[0][2]}
    -----------
@@ -13,10 +14,28 @@ function GameBoard() {
    -----------
     ${board[2][0]} | ${board[2][1]} | ${board[2][2]}`;
 
-		console.log((boardDisplay));
-	}; //function()
+		console.log(boardDisplay);
+	}; //printBoard()
 
-	return { printBoard };
+	const resetBoard = function resetBoard() {
+		for (let row = 0; row < 3; row++) {
+			for (let col = 0; col < 3; col++) {
+				board[row][col] = "";
+			} //for()
+		} //for()
+	}; //resetBoard()
+
+	const getBoard = function getBoard() {
+		return board;
+	}; //getBoard()
+
+	return { printBoard, resetBoard, getBoard };
 } //GameBoard()
 
-GameBoard().printBoard();
+const board = GameBoard();
+
+board.printBoard();
+
+board.resetBoard();
+
+board.printBoard();
